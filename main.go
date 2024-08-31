92577400a36c7de3c9a2493e39b88545f6420f16
qlack <qlack@qlacks-MacBook-Pro.local>
2024-08-31 14:32:28 +0700

    add proxy

--- main.go
+++ main.go
@@ -3,23 +3,42 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 
 	"github.com/phatvo2201/pkg/tcp_helper"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", ":30010")
+	// Listen on local port 80
+	listener, err := net.Listen("tcp", ":80")
 	if err != nil {
 		log.Fatalln("Unable to bind to port")
 	}
-	log.Println("Listening on 0.0.0.0:30010")
+	b := []byte{}
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Fatalln("fail when connect to port")
-
+			log.Fatalln("Unable to accept connection")
 		}
-		go tcp_helper.Echo(conn)
-	}
 
+		os.Stdout.Write(b)
+		go tcp_helper.ProxyHandler(conn)
+	}
 }
+
+// func main() {
+// 	listener, err := net.Listen("tcp", ":30010")
+// 	if err != nil {
+// 		log.Fatalln("Unable to bind to port")
+// 	}
+// 	log.Println("Listening on 0.0.0.0:30010")
+// 	for {
+// 		conn, err := listener.Accept()
+// 		if err != nil {
+// 			log.Fatalln("fail when connect to port")
+
+// 		}
+// 		go tcp_helper.Echo(conn)
+// 	}
+
+// }
--- pkg/tcp_helper/proxy.go
+++ pkg/tcp_helper/proxy.go
@@ -2,8 +2,13 @@ package tcp_helper
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net"
 	"os"
 )
+
+var desAddress = "qlack.com.vn"
 
 type Reader struct{}
 
@@ -17,4 +22,27 @@ type FooWriter struct{}
 func (fooWriter *FooWriter) Write(b []byte) (int, error) {
 	fmt.Print("out> ")
 	return os.Stdout.Write(b)
+}
+
+func ProxyHandler(src net.Conn) {
+
+	des, err := net.Dial("tcp", desAddress)
+	if err != nil {
+		log.Println("fail when connect to destination service ...")
+		return
+	}
+	go func() {
+		size, err := io.Copy(des, src)
+		if err != nil {
+			log.Fatal()
+		}
+		log.Printf("forward %d to destination", size)
+
+	}()
+	if _, err := io.Copy(src, des); err != nil {
+		log.Println("fail when send output back to source service ...")
+		return
+
+	}
+
 }
